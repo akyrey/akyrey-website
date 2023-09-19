446fe25af52b65ce36572e4c77e966e17130584c
akyrey <stein.dario@gmail.com>
2023-09-20 00:28:18 +0200

    feat(*): wip add image, first section and them switch

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        secondary: getColorFromVariable("--secondary"),
+        tertiary: getColorFromVariable("--tertiary"),
+        main: getColorFromVariable("--main"),
+      },
+    },
+  },
+  plugins: [],
+};
+
+function getColorFromVariable(value) {
+  return function (opacity) {
+    if (opacity.opacityValue !== undefined) {
+      return `rgba(var(${value}), ${opacity.opacityValue})`;
+    }
+
+    if (opacity.opacityVariable !== undefined) {
+      return `rgba(var(${value}), var(${opacity.opacityVariable}, 1))`;
+    }
+
+    return `rgb(var(${value}))`;
+  };
 }
