fa19d6976771392d080510c4b4aa14e23e532446
Dario Vogogna <dario.vogogna@euronovate.com>
2023-10-22 00:13:26 +0200

    style(index): wip add simpe animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -10,9 +11,33 @@ module.exports = {
         light: getColorFromVariable("--light"),
         dark: getColorFromVariable("--dark"),
       },
+      keyframes: {
+        fadein: {
+          from: { transform: 'translateY(2.5rem)', opacity: 0 },
+          to: { transform: 'translateY(0px)', opacity: 1 },
+        },
+      },
+      animation: {
+        fadein: 'fadein 0.8s ease-in-out',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          "animation-delay": (value) => {
+            return {
+              "animation-delay": value,
+            };
+          },
+        },
+        {
+          values: theme("transitionDelay"),
+        }
+      );
+    }),
+  ],
 };
 
 function getColorFromVariable(value) {
